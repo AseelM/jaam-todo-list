75d1774194b2a83f432a9facd738565018df8573
RogeredBacon <jinglis12@googlemail.com>
2017-07-05 16:45:37 +0100

    Return new array and should contain todos tests #1

--- test.js
+++ test.js
@@ -1,8 +1,17 @@
 var test = require('tape');
 var logic = require('./logic');
 
-var todos = [];
+var todos = [4,5];
 var todosObject = [{}, {}];
+var newTodos = [2,7,4,5];
+
+var newTodosIncludes = function() {
+  var present = true;
+  for(var i = 0;i < todos.length && present;i++) {
+    present = newTodos.includes(todos[i]);
+    }
+  return present;
+  }
 
 test('Tape runs okay', function(t) {
   t.equal(1, 1, 'One should equal one');
@@ -20,5 +29,19 @@ test('Todos array contains only objects', function(t) {
   var actual = (!Array.isArray(todosObject[0]) && typeof todosObject[0] === 'object');
   var expected = true;
   t.deepEqual(actual, expected, 'Todos array should only contain objects');
+  t.end();
+});
+
+test('addTodo returns a new array', function(t) {
+  var actual = newTodos.length !== todos.length;
+  var expected = true;
+  t.deepEqual(actual, expected, 'addTodo should return a new array!');
+  t.end();
+});
+
+test('newTodos should contain todos', function(t) {
+  var actual = newTodosIncludes();
+  var expected = true;
+  t.equal(actual, expected, 'newTodos should contain todo!');
   t.end();
 });

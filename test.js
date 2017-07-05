f84d36f63c909bd5891ff618b6666e09e4da5bee
ameliejyc <amelie.chan@gmail.com>
2017-07-05 15:23:30 +0100

    created initial tests Relates #1

--- test.js
+++ test.js
@@ -1,7 +1,24 @@
 var test = require('tape');
 var logic = require('./logic');
 
-test('Example test', function(t) {
-  t.pass();
+var todos = [];
+var todosObject = [{}, {}];
+
+test('Tape runs okay', function(t) {
+  t.equal(1, 1, 'One should equal one');
+  t.end(); // this tells tape we've finished
+});
+
+test('Todos is an array', function(t) {
+  var actual = Array.isArray(todos);
+  var expected = true;
+  t.deepEqual(actual, expected, 'Todos should be an array');
+  t.end();
+});
+
+test('Todos array contains only objects', function(t) {
+  var actual = (!Array.isArray(todosObject[0]) && typeof todosObject[0] === 'object');
+  var expected = true;
+  t.deepEqual(actual, expected, 'Todos array should only contain objects');
   t.end();
 });

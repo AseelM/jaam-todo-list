f75bc67749757839a979fc2be00dfb53dc997e76
RogeredBacon <jinglis12@googlemail.com>
2017-07-05 17:06:40 +0100

    addTodo tests written #1

--- test.js
+++ test.js
@@ -1,17 +1,18 @@
 var test = require('tape');
 var logic = require('./logic');
 
-var todos = [4,5];
+var todos = [4, 5];
 var todosObject = [{}, {}];
-var newTodos = [2,7,4,5];
+var newArr = [2, 7, 4, 5, 9];
+var newTodo = [9];
 
-var newTodosIncludes = function() {
+var newArrIncludes = function() {
   var present = true;
-  for(var i = 0;i < todos.length && present;i++) {
-    present = newTodos.includes(todos[i]);
-    }
-  return present;
+  for (var i = 0; i < todos.length && present; i++) {
+    present = newArr.includes(todos[i]);
   }
+  return present;
+}
 
 test('Tape runs okay', function(t) {
   t.equal(1, 1, 'One should equal one');
@@ -33,15 +34,22 @@ test('Todos array contains only objects', function(t) {
 });
 
 test('addTodo returns a new array', function(t) {
-  var actual = newTodos.length !== todos.length;
+  var actual = newArr.length !== todos.length;
   var expected = true;
   t.deepEqual(actual, expected, 'addTodo should return a new array!');
   t.end();
 });
 
-test('newTodos should contain todos', function(t) {
-  var actual = newTodosIncludes();
+test('newArr should contain todos', function(t) {
+  var actual = newArrIncludes();
   var expected = true;
-  t.equal(actual, expected, 'newTodos should contain todo!');
+  t.equal(actual, expected, 'newArr should contain todo!');
+  t.end();
+});
+
+test('newTodo added to the end of newArr', function(t) {
+  var actual = (newArr.length > todos.length && newArr[newArr.length - 1] !== todos[todos.length - 1] && newArr[newArr.length - 1] === newTodo[newTodo.length - 1]);
+  var expected = true;
+  t.deepEqual(actual, expected, 'newTodo needs to finish newArr!');
   t.end();
 });
